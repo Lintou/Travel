0af8070a312296b2f28c6204bfab2c61c5b11c57
Lintou <1139250339@qq.com>
2019-06-23 19:13:58 +0800

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-
     <router-view/>
   </div>
 </template>
@@ -10,9 +9,6 @@ export default {
   name: 'App'
 }
 </script>
-
 <style>
-#app {
- 
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
+/* eslint-disable */
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'// 移动端1像素边框
-import fastClick from 'fastclick'// click事件延迟300ms  npm install fastclick --save
-Vue.config.productionTip = false
+import './assets/styles/iconfont.css'
 
+Vue.config.productionTip = false
+fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
