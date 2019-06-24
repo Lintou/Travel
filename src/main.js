e3d0910cc091142dd99ba4828073cae0e74fbf64
Lintou <1139250339@qq.com>
2019-06-24 11:03:25 +0800

    changeswiper

--- src/main.js
+++ src/main.js
@@ -6,9 +6,12 @@ import fastClick from 'fastclick'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'// 移动端1像素边框
 import './assets/styles/iconfont.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper,/*{default global options}*/)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class='wrapper'>
+    <swiper :options="swiperOption">
+        <!-- slides -->
+        <swiper-slide v-for='item of swiperList' :key='item.id'>
+        <img class='swiper-img' :src='item.imgUrl'/>
+        </swiper-slide>
+        <!-- Optional controls -->
+        <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl:'https://hbimg.huabanimg.com/f83c242c6987e11073ca9badeb3cfcba3b3a4487297ddf-PIwfmx_fw658'
+      },
+      {
+        id: '0002',
+        imgUrl: 'https://hbimg.huabanimg.com/b389355be29da3836d2fc29493be3f8da657a9f59fc1a-WcJ4gg_fw658'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #ffffff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%  
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
