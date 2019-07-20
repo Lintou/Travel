c9b6ba80c1a3860a7690d08f96a3a6aabc97da4a
Lintou <1139250339@qq.com>
2019-07-20 14:28:15 +0800

    banner finish

---
+++ src/common/gallery/Gallery.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container" @click="handleGalleryClick">
+    <div class="wrapper">
+      <swiper :options="swiperOptions">
+        <!-- slides -->
+        <swiper-slide v-for="(item, index) in imgs" :key="index">
+          <img class='gallery-img' :src='item'/>
+        </swiper-slide>
+        <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CommonGallery',
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return ['https://hbimg.huabanimg.com/1b9b90b05099e1d64455aef4bcbdd5faf919b68e2e082-WT6uaQ_fw658', 
+        'https://hbimg.huabanimg.com/35258a3f1e4cf6bc4be583d45af964f8abe62e7e22ef8-3W1OzW_fw658']
+      }
+    }
+  },
+  methods: {
+    handleGalleryClick () {
+      this.$emit('close')
+    }
+  },
+  data () {
+    return {
+      swiperOptions: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        observer: true,
+        observerParents: true,
+        autoplay: false
+      }
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .container >>> .swiper-container
+    overflow: inherit
+  .container
+    display: flex
+    flex-direction: column
+    justify-content: center
+    z-index: 99
+    position :fixed
+    left: 0
+    right: 0
+    top: 0
+    bottom: 0
+    background: #000
+    .wrapper
+      width: 100%
+      height: 0
+      padding-bottom: 100%
+      .gallery-img
+        width: 100%
+      .swiper-pagination
+        color: #ffffff
+</style>
